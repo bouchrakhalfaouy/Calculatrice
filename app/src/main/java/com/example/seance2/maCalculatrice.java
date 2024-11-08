1023ac4e7b1e66de991c2bf83822c04e964d4295
bouchrakhalfaouy <bkhalfaouy@gmail.com>
2024-11-08 17:02:36 +0100

    commit2

--- app/src/main/java/com/example/seance2/maCalculatrice.java
+++ app/src/main/java/com/example/seance2/maCalculatrice.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
